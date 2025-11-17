63f2dd5d7ca5e36d41740d6a3e97bd1143c4cafe
Yevhenii <eugene.bilan@gmail.com>
2025-11-17 22:07:35 +0100

    update images

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -38,7 +38,7 @@ const getLinkClass = (path: string) => {
         <nuxt-img
           class="w-16"
           loading="lazy"
-          src="/logo.png"
+          src="logo.png"
           alt="WP Content Scraper"
         />
       </nuxt-link>
